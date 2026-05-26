e12d534ac3f670a48d8350fd9ee7eb6a7111e945
abbi portwood <apwood09@gmail.com>
2026-05-26 13:52:23 -0400

    final solution

--- server/app.py
+++ server/app.py
@@ -13,10 +13,28 @@ app = create_app(env)
 
 class Books(Resource):
     def get(self):
-        books = [BookSchema().dump(b) for b in Book.query.all()]
-        return books, 200
+        # accept query parameters 
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per_page', default=5, type=int)
 
+        # use .paginate() in query
+        # error_out=False: prevent crashing, user types page doesn't exist
+        pagination = Book.query.paginate(page=page, per_page=per_page, error_out=False)
 
+        # return: structured response 
+        # marshmallow schema: format items (books)
+        books_data = [BookSchema().dump(b) for b in pagination.items]
+
+        response_dict = {
+            "page": pagination.page,
+            "per_page": pagination.per_page,
+            "total": pagination.total,
+            "total_pages": pagination.pages,
+            "items": books_data
+        }
+        
+        return make_response(jsonify(response_dict), 200)
+        
 api.add_resource(Books, '/books', endpoint='books')
 
 
